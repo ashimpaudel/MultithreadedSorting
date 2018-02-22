7caadb59950c0ccfbc713d89f97e2e3b4b6ee83a
ashimpaudel <ashimpaudel@gmail.com>
2018-02-21 23:11:17 -0500

    main function complete

--- multisort.c
+++ multisort.c
@@ -3,90 +3,41 @@
 #include <stdlib.h>
 
 
-int sum; /* this data is shared by the thread(s) */
+int sum; /* this data is shared by the thread(s) we create */
 void *sorter(void *param); /* threads call this function */
 void merge(int low, int high);
 
-int array[] = {99,9,97,23,45,32,904,5,66,60}; //our target array
+int sortArray[] = {99,9,97,23,45,32,904,5,66,60}; //our target array
 
+typedef struct{
+    int first;
+    int last;
+}fLIndices;
 
-//structure to keep the index of array
-typedef struct 
-{
-	int high;
-	int low;
-}Index;
-
-int main(int argc, char *argv[])
-{
-	Index in;
-	in.low = 0;
-	in.high = 10;
-    pthread_t thread; /* 3 thread identifier */
-    pthread_create(&thread, NULL, sorter, &in); //For last argument pthread_create takes the address of any type
-    pthread_join(thread,NULL);
+int main(int argc, char *argv[]){
     
+    pthread_t th1, th2; //declaring two threads, later on we pass the index of two halves of array to each of them
+    
+    //threads are created by pthread_create function, we need to pass arguements tho this function of type void pointer
+    //next step is to create two arguements to pass to each of thread
+    
+    fLIndices args1, args2;
+    
+    args1 -> first = 0
+    //finding the middle point of the array
+    middlePoint = (sizeof(sortArray)/sizeof(sortArray[0]))/2 
+    args1 -> last = middlePoint
 
-    int i;
-    for (i = 0; i < 10; i++) printf ("%d ", array[i]);
+    args2 -> first = middlePoint + 1;
+    args2 -> last = (sizeof(sortArray)/sizeof(sortArray[0])) - 1;
+    
+    //creating two threads 
+    pthread_create(&th1, NULL, mergeSort, &args1);
+    pthread_create(&th2, NULL, mergeSort, &args2);
+    
+    void pthread_join(th1, NULL);
+    void pthread_join(th2, NULL);
     return 0;
-    
-}
-/* The thread will begin control in this function */
-void *sorter(void *index_info)
-{
-	Index *ind = (Index *)index_info;
-	int mid = (ind->low + ind->high)/2;
-
-	Index nIndex[2];
-	pthread_t threads[2];
-
-	nIndex[0].low = ind->low;
-	nIndex[0].high = mid;
-
-	nIndex[1].low = mid+1;
-	nIndex[1].high = ind->high;
-
-	if (ind->low < ind->high){
-		pthread_create(&threads[0], NULL, sorter, &nIndex[0]);
-		pthread_create(&threads[1], NULL, sorter, &nIndex[1]);
-
-		pthread_join(threads[0], NULL);
-		pthread_join(threads[1], NULL);
-
-		merge(ind->high, ind->low);
-
-    	pthread_exit(0);
-
-	}
 
 }
 
-void merge(int low, int high){
-	int mid = (low+high)/2;
-	int left = low;
-	int right = mid+1;
-
-	int new_arr[high-low+1];
-	int i;
-	int curr = 0;
-
-	while(left <= mid && right <= high){
-		if (array[left] > array[right])
-			new_arr[curr++] = array[right++];
-		else
-			new_arr[curr++] = array[right++];
-	}
-
-	while(left <= mid) 
-		new_arr[curr++] = array[left++];
-	while(right <= high) 
-		new_arr[curr++] = array[left++];
-	for (i = 0; i < (high-low+1); i++)
-		 array[low+i] = new_arr[i];
-		 //printf("%d\n",array[low+i] );
-
-
-
-
-}
